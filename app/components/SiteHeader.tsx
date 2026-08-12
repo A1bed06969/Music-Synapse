387d3918ced0d4bd5c25c69fe24d0ca6ae6699e8
TH <a1bed069star@gmail.com>
2026-08-12 18:01:21 +0900

    feat: add festivals & events link to the header nav

--- app/components/SiteHeader.tsx
+++ app/components/SiteHeader.tsx
@@ -4,6 +4,7 @@ const NAV_LINKS = [
   { href: '/', label: 'ホーム' },
   { href: '/search', label: '検索' },
   { href: '/relations', label: '相関図' },
+  { href: '/events', label: 'フェス&イベント' },
   { href: '/map', label: 'マップ' },
   { href: '/media', label: 'メディア' },
   { href: '/admin/import', label: 'iTunes登録' },
